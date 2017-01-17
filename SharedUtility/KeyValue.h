c82ef8632fea693c2e4398fe1936ca8a6ae06a59
Joyeewen <wenzeyi@gmail.com>
2017-01-17 22:40:35 +1100

    add data reader files

---
+++ SharedUtility/KeyValue.h
@@ -0,0 +1,33 @@
+/*
+ * keyValue.h
+ *
+ *  Created on: 9 Mar 2016
+ *      Author: Zeyi Wen
+ *		@brief: 
+ */
+
+#ifndef KEYVALUE_H_
+#define KEYVALUE_H_
+
+#include <vector>
+#include "DataType.h"
+
+
+using std::vector;
+
+class KeyValue
+{
+public:
+	float_point featureValue;
+	int id;//This id may be used as feature id or instance id
+
+public:
+	static void SortFeaValue(int nNumofDim, vector<vector<KeyValue> > &vvInsSparse, vector<vector<KeyValue> > &vvFeaInxPair);
+	static void VecToArray(vector<vector<KeyValue> > &vvFeaInxPair, int *pInsId, float_point *pdValue, int *pNumofKeyValue, long long *plStartPos);
+
+	static void TestVecToArray(vector<vector<KeyValue> > &vvFeaInxPair, int *pInsId, float_point *pdValue, int *pNumofKeyValue);
+};
+
+
+
+#endif /* KEYVALUE_H_ */
