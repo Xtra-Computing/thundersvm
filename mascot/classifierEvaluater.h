e9016a9d049877b29ea708de10225e358f6eb8ce
Joyeewen <wenzeyi@gmail.com>
2017-01-21 01:31:34 +1100

    organise evaluater

--- mascot/classifierEvaluater.h
+++ mascot/classifierEvaluater.h
@@ -19,45 +19,9 @@ public:
 	static vector<float_point> trainingError;
 	static vector<float_point> testingError;
 public:
-	/**
-	 * @brief: evaluate sub-classifiers for multi-class classification
-	 */
-	static void evaluateSubClassifier(const vector<vector<int> > &missLabellingMatrix, vector<float_point> &vErrorRate){
-		vErrorRate.clear();
 
-		int row = missLabellingMatrix.size();
-		int col = missLabellingMatrix[0].size();
-		int totalIns = 0, totalMiss = 0;
-		for(int r = 0; r < row; r++){
-			totalIns += missLabellingMatrix[r][r];
-			for(int c = r + 1; c < col; c++){
-				int totalRC = missLabellingMatrix[r][r] + missLabellingMatrix[c][c];
-				int rcMissLabelling = missLabellingMatrix[r][c] + missLabellingMatrix[c][r];
-				totalMiss += rcMissLabelling;
-				//printf("%d and %d accuracy is %f\n", r, c, (float)rcMissLabelling / totalRC);
-				vErrorRate.push_back((float)rcMissLabelling / totalRC);
-			}
-		}
-	    printf("classifier incorrect rate = %.2f%%(%d/%d)\n", totalMiss / (float) totalIns * 100,
-	    		totalMiss, totalIns);
-	}
-
-	/*
-	 * @brief: update C to better fit the problem.
-	 */
-	static vector<float_point> updateC(const vector<float_point> &vOldC){
-		vector<float_point> vNewC;
-		for(uint i = 0; i < trainingError.size(); i++){
-			if(trainingError[i] < testingError[i]){
-				vNewC.push_back(vOldC[i] * 0.5);//reduce C
-			}else if(trainingError[i] > testingError[i]){
-				vNewC.push_back(vOldC[i] * 2);//increase C
-			}else{
-				vNewC.push_back(vOldC[i]);
-			}
-		}
-		return vNewC;
-	}
+	static void evaluateSubClassifier(const vector<vector<int> > &missLabellingMatrix, vector<float_point> &vErrorRate);
+	static vector<float_point> updateC(const vector<float_point> &vOldC);
 };
 
 
