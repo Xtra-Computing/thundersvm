dc555f4a24f127308e5193bebbb04396bd4a1785
Zeyi Wen <wenzeyi@gmail.com>
2017-06-20 14:49:48 +0800

    improve reduction

--- SharedUtility/getMin.cu
+++ SharedUtility/getMin.cu
@@ -1,7 +1,10 @@
+#include <stdio.h>
 #include "getMin.h"
 
 
 __device__ int getBlockMin(const float *values, int *index) {
+	if(blockDim.x % 32 != 0)
+		printf("Warning: block size isn't suited to reduction. getBlockMin may have error! #################\n");
 	int tid = threadIdx.x;
 	index[tid] = tid;
 	__syncthreads();
@@ -16,22 +19,10 @@ __device__ int getBlockMin(const float *values, int *index) {
 	return index[0];
 }
 
-__device__ void GetMinValueOriginal(real *pfValues, int *pnKey)
-{
-	//Reduce by a factor of 2, and minimize step size
-	for (int i = blockDim.x / 2; i > 0 ; i >>= 1) {
-		int tid = threadIdx.x;
-		if (tid < i)
-			if (pfValues[tid + i] < pfValues[tid]) {
-                pfValues[tid] = pfValues[tid + i];
-                pnKey[tid] = pnKey[tid +i];
-            }
-        __syncthreads();
-	}
-}
-
 __device__ void GetMinValueOriginal(real *pfValues)
 {
+	if(blockDim.x % 32 != 0)
+		printf("Warning: block size isn't suited to reduction. getBlockMin may have error! #################\n");
 	//Reduce by a factor of 2, and minimize step size
 	for (int i = blockDim.x / 2; i > 0 ; i >>= 1) {
 		int tid = threadIdx.x;
