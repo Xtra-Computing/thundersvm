6d3f33586562f48a4292d492769ac1d07828c8f0
Jiashuai Shi <shijiashuai@gmail.com>
2017-11-01 15:25:54 +0800

    add metric

---
+++ src/thundersvm/util/metric.cpp
@@ -0,0 +1,31 @@
+//
+// Created by jiashuai on 17-11-1.
+//
+#include <thundersvm/util/metric.h>
+
+string Accuracy::name() {
+    return "Accuracy";
+}
+
+real Accuracy::score(const vector<real> &predict_y, const vector<real> &ground_truth_y) {
+    int n_correct = 0;
+    for (int i = 0; i < predict_y.size(); ++i) {
+        if (predict_y[i] == ground_truth_y[i])
+            n_correct++;
+    }
+    float accuracy = n_correct / (float) ground_truth_y.size();
+    return accuracy;
+}
+
+string MSE::name() {
+    return "Mean Squared Error";
+}
+
+real MSE::score(const vector<real> &predict_y, const vector<real> &ground_truth_y) {
+    real mse = 0;
+    for (int i = 0; i < predict_y.size(); ++i) {
+        mse += (predict_y[i] - ground_truth_y[i]) * (predict_y[i] - ground_truth_y[i]);
+    }
+    mse /= predict_y.size();
+    return mse;
+}
