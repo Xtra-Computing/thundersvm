7ce877e90abf482c50e561ed8c589d704cf93eb9
Jiashuai Shi <shijiashuai@gmail.com>
2017-12-31 11:49:57 +0800

    set default gamma #35

--- src/thundersvm/thundersvm-train.cpp
+++ src/thundersvm/thundersvm-train.cpp
@@ -53,6 +53,11 @@ int main(int argc, char **argv) {
             }
         }
 
+        if (parser.param_cmd.gamma == 0 && parser.param_cmd.kernel_type != SvmParam::LINEAR){
+            parser.param_cmd.gamma = 1.f / train_dataset.n_features();
+            LOG(WARNING)<<"using default gamma="<<parser.param_cmd.gamma;
+        }
+
 #ifdef USE_CUDA
         CUDA_CHECK(cudaSetDevice(parser.gpu_id));
 #endif
