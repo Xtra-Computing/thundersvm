a524e6af138839f0f2215215cd545f1c05056dd7
Li Qinbin <godqinbin@gmail.com>
2018-10-28 12:55:10 +0800

    fix bug on set memory size

--- src/thundersvm/model/svmmodel.cpp
+++ src/thundersvm/model/svmmodel.cpp
@@ -338,5 +338,5 @@ int SvmModel::get_working_set_size(int n_instances, int n_features) {
 
 void SvmModel::set_max_memory_size(size_t size) {
     if(size > 0)
-		this->param.max_mem_size = static_cast<size_t>(size) << 20;
+		this->param.max_mem_size = static_cast<size_t>(size);
 }
