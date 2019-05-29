205aafc3aec06fdf92e3b498af03643a6901de50
jiahuanluo <luojiahuan001@gmail.com>
2019-05-29 14:07:28 +0800

    update __init__.py

--- python/thundersvm/__init__.py
+++ python/thundersvm/__init__.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 # -*-coding:utf-8-*-
 """
- * Copyright (C) 2018 OwnThink.
- *
  * Name        : __init__.py
  * Author      : Locke <luojiahuan001@gmail.com>
  * Version     : 0.0.1
