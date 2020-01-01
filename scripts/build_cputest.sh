943a0989dc2f17092fa294b1abf354575397949e
Jiashuai Shi <shijiashuai@gmail.com>
2020-01-01 21:11:29 +0800

    add macos build
    macos build
    
    macos build

--- scripts/build_cputest.sh
+++ scripts/build_cputest.sh
@@ -10,6 +10,17 @@ linux)
     cmake -DBUILD_TESTS=ON -DUSE_CUDA=OFF -DUSE_EIGEN=ON ..
     ;;
 osx)
+    brew install cmake libomp
+    cmake \
+    -DUSE_CUDA=OFF \
+    -DUSE_EIGEN=ON \
+    -DOpenMP_C_FLAGS="-Xpreprocessor -fopenmp -I/usr/local/opt/libomp/include" \
+    -DOpenMP_C_LIB_NAMES=omp \
+    -DOpenMP_CXX_FLAGS="-Xpreprocessor -fopenmp -I/usr/local/opt/libomp/include" \
+    -DOpenMP_CXX_LIB_NAMES=omp \
+    -DOpenMP_omp_LIBRARY=/usr/local/opt/libomp/lib/libomp.dylib \
+    -DBUILD_TESTS=ON \
+    ..
    ;;
 windows)
     export PATH=${MSBUILD_PATH}:$PATH
